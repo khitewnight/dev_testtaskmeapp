380a4aeb321d028b06773c330b168733df9fcde6
geoboom <leegeorgie52@gmail.com>
2018-03-04 21:28:52 +0800

    added eslint-disable and removed commented code

--- be/index.js
+++ be/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console, no-unused-vars */
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
@@ -14,34 +15,6 @@ const app = express();
 app.get('/test', (req, res) => {
   res.send('hello');
 });
-
-// app.post('/schedule', cors(), (req, res) => {
-//   // const jobArr = await parseSchedule(req.file.buffer);
-//   console.log('Received request');
-
-//   upload.single('schedule')(req, res, async (err) => {
-//     if (err) {
-//       console.log(err);
-
-//       if (req.fileFormatError) {
-//         res.status(500).send({ error: req.fileFormatError });
-//         console.log(req.fileFormatError);
-//         return;
-//       }
-
-//       res.status(500).send({ error: 'Error in file upload.' });
-//       return;
-//     }
-
-//     try {
-//       const jobArr = await parseSchedule(req.file.buffer);
-//       res.json(jobArr);
-//     } catch (e) {
-//       console.log(`Error parsing excel file: ${e}`);
-//       res.status(500).send({ error: 'Error parsing excel file.' });
-//     }
-//   });
-// });
 
 app.post(
   '/schedule',
